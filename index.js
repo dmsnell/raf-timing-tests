1a40d48bfdbe7a34bfb6a628ecc78805362d93f6
Dennis Snell <dennis.snell@automattic.com>
2016-04-10 07:38:57 +0100

    Fix timing bug in `onRaf` > `afterRaf` > `onRaf` scheduling
    Previously we scheduled the next `onRaf` from within `afterRaf`. This
    was problematic because it led to skipping `onRaf` accidentally when the
    delay was small enough.
    
    Now the next run is scheduled from within `onRaf` itself and things are
    working as expected.

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
 'use strict'
 
-const domReady = () => {
-  console.log( 'Calibrating at 23ms' )
-  timed( () => delayBy( 23 ) )
-
-  requestAnimationFrame( onRaf )
-  requestAnimationFrame( frameDelayer( 500 ) )
+const mutator = {
+  delayCount: 0,
+  frameCount: 0,
+  rafCount: 0,
 }
 
-const frameDelayer = ms => () => {
-  delayBy( ms )
+const domReady = () => {
+  requestAnimationFrame( onRaf )
+  requestAnimationFrame( frameDelayer( 230 ) )
+  requestAnimationFrame( frameCounter( performance.now() ) )
+}
+
+const fpsChunk = 10
+const frameCounter = tic => () => {
+  mutator.frameCount += 1
+  const count = mutator.frameCount
+
+  if ( count > 0 && ! ( count % fpsChunk ) ) {
+    const toc = performance.now()
+    console.log( `${ fpsChunk / ( toc - tic ) * 1000 } fps` )
+    return requestAnimationFrame( frameCounter( performance.now() ) )
+  }
+
+  requestAnimationFrame( frameCounter( tic ) )
+}
+
+const frameDelayer = ms => ( phantom = 0 ) => {
+  mutator.delayCount += delayBy( ms )
 
   requestAnimationFrame( frameDelayer( ms ) )
 }
 
 const afterRaf = tic => {
-  sayDelay( tic )
-
-  requestAnimationFrame( onRaf )
-}
-
-const onRaf = () => {
-  const tic = performance.now()
-
-  setTimeout( () => afterRaf( tic ) )
-}
-
-const sayDelay = tic => {
   const toc = performance.now()
 
   console.log( `Δ = ${ toc - tic }` )
 }
 
-const timed = fn => {
+const onRaf = () => {
+  mutator.rafCount += 1
   const tic = performance.now()
-  const val = fn()
-  const toc = performance.now()
 
-  console.log( `took ${ toc - tic } ms` )
-  return val
+  setTimeout( () => afterRaf( tic ), 0 )
+
+  requestAnimationFrame( onRaf )
 }
 
 const delayBy = ms => {
   const tic = performance.now()
+  let count = 0
   let toc = performance.now()
 
   while ( ( toc - tic ) < ms ) {
+    count += 1
     toc = performance.now()
   }
+
+  return count
 }
 
 document.addEventListener( 'DOMContentLoaded', domReady )
